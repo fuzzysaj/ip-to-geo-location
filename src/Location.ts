8368a82c45b5f373cea9905f2c12e578febd9d7b
fuzzysaj <fuzzysaj@gmail.com>
2019-07-10 16:55:27 -0700

    adding region types

--- src/Location.ts
+++ src/Location.ts
@@ -1,9 +1,11 @@
+export type RegionType = 'state' | 'province' | 'district' | 'other'
+
 export interface Location {
   country: string,
   country_code: string,
   region: string,
   region_code: string,
-  region_type: string,
+  region_type: RegionType,
   city: string
   lat: number,
   lon: number
